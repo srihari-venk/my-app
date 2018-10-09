44bf8a61b4ca7e49041ec8501c9ba0bd465b1146
Sri Hari V <srihari.venk@gmail.com>
2018-10-09 19:36:04 +0800

    Completed till Routing

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  constructor(private heroService: HeroService) { }
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+
+  heroes: Hero[];
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
